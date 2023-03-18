2d64e4b1f827cd1f804900b2b8a71bba9ae38992
Timofey Nosenkov <116382351+timih18@users.noreply.github.com>
2023-03-18 20:24:34 +0300

    Add files via upload

--- ball.py
+++ ball.py
@@ -1,28 +1,38 @@
+# импортируем pygame и randint
 import pygame
 from random import randint
 
 
+# определяем черный цвет в rgb
 black = (0, 0, 0)
 
 
+# создадим класс мяч он будет происходить от sprite в pygame
 class Ball(pygame.sprite.Sprite):
+    # зададим параметры мяча
     def __init__(self, color, width, height):
+        # вызовем родительский класс sprite
         super().__init__()
 
+        # создадим картинку мяча и сделаем ее по умолчанию прозрачной
         self.image = pygame.Surface([width, height])
         self.image.fill(black)
         self.image.set_colorkey(black)
 
         pygame.draw.rect(self.image, color, [0, 0, width, height])
 
+        # создфдим массив с векторами скоростей по осям x и y
         self.velocity = [randint(4, 8), randint(-8, 8)]
 
+        # создадим обЪект мяч по картинке
         self.rect = self.image.get_rect()
 
+    # создадим функцию для движения мяча
     def update(self):
         self.rect.x += self.velocity[0]
         self.rect.y += self.velocity[1]
 
+    # создадим функцию для осткока
     def bounce(self):
         self.velocity[0] = -self.velocity[0]
         self.velocity[1] = randint(-8, 8)
--- pong.py
+++ pong.py
@@ -1,46 +1,61 @@
+# импортируем pygame, ball и paddle
 import pygame
-from paddle import Paddle
 from ball import Ball
+from paddle import Paddle
 
+
+# запустим pygame
 pygame.init()
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+# определим черный и белый цвета в rgb
+black = (0, 0, 0)
+white = (255, 255, 255)
 
+# создадим окно
 size = (700, 500)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Pong")
+pygame.display.set_caption('Pong')
 
-paddleA = Paddle(WHITE, 10, 100)
+# создадим две ракетки
+paddleA = Paddle(white, 10, 100)
 paddleA.rect.x = 20
 paddleA.rect.y = 200
 
-paddleB = Paddle(WHITE, 10, 100)
+paddleB = Paddle(white, 10, 100)
 paddleB.rect.x = 670
 paddleB.rect.y = 200
 
-ball = Ball(WHITE, 10, 10)
+# создадим мяч
+ball = Ball(white, 10, 10)
 ball.rect.x = 345
 ball.rect.y = 195
 
-all_sprites_list = pygame.sprite.Group()
+# создадим лист спрайтов и добавим в него мяч и ракетки
+sprites_list = pygame.sprite.Group()
+sprites_list.add(paddleA)
+sprites_list.add(paddleB)
+sprites_list.add(ball)
 
-all_sprites_list.add(paddleA)
-all_sprites_list.add(paddleB)
-all_sprites_list.add(ball)
+# создадим переменную которая будет отвечать за ссостояние окна (открыто/закрыто)
+on = True
 
-carryOn = True
-
+# создадим часы которые будут контролировать обновление экрана
 clock = pygame.time.Clock()
 
+# создадим счетчики
 scoreA = 0
 scoreB = 0
 
-while carryOn:
+# создадим основной цикл игры
+while on:
+    # создадим условие закрытия окна
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            carryOn = False
+            on = False
 
+    # зададим кнопки движения ракеток
+    # игрок А - w/s
+    # игрок B - UP/DOWN
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         paddleA.move_up(5)
@@ -51,8 +66,9 @@ while carryOn:
     if keys[pygame.K_DOWN]:
         paddleB.move_down(5)
 
-    all_sprites_list.update()
+    sprites_list.update()
 
+    # отскок от стен
     if ball.rect.x >= 690:
         scoreA += 1
         ball.velocity[0] = -ball.velocity[0]
@@ -64,22 +80,24 @@ while carryOn:
     if ball.rect.y < 0:
         ball.velocity[1] = -ball.velocity[1]
 
+    # определение столкновений с ракетками
     if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
         ball.bounce()
 
-    screen.fill(BLACK)
-    pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
-
-    all_sprites_list.draw(screen)
-
+    # создадим окно
+    screen.fill(black)
+    pygame.draw.line(screen, white, [349, 0], [349, 500], 5)
+    sprites_list.draw(screen)
+    # счетчики
     font = pygame.font.Font(None, 74)
-    text = font.render(str(scoreA), 1, WHITE)
+    text = font.render(str(scoreA), 1, white)
     screen.blit(text, (250, 10))
-    text = font.render(str(scoreB), 1, WHITE)
+    text = font.render(str(scoreB), 1, white)
     screen.blit(text, (420, 10))
-
     pygame.display.flip()
 
+    # устоновим частоту обновления экрана
     clock.tick(60)
 
+# выйдем из pygame
 pygame.quit()
